745b6766312795059ea8263ed55097df12ce17a4
coderbabu00 <coderbabu00@gmail.com>
2024-05-05 21:41:19 -0700

    first commit

---
+++ 01Variables.js
@@ -0,0 +1,13 @@
+// Var const and let
+  var c = 30;
+// var=> Which can be changed further.
+// const=> Which can not be changed.
+// const a =10;
+// let=> Which can be changed further.
+let b = 20;
+
+a=20;
+b=30;
+c=40;
+
+console.table([a,b,c])
